a24a4358c1e8aaec27d8023ca79621f680c55902
bharatsush <suresheca2012@gmail.com>
2016-01-25 14:18:26 +0530

    It is si2chip cloud

---
+++ myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/si2chip/myproject/myprojectenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ si2chip_cloud/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Si2ChipCloudConfig(AppConfig):
+    name = 'si2chip_cloud'
