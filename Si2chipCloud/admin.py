2c540c1004ee6ad0ae94ca8c22aca84c6e683e7f
bharatsush <suresheca2012@gmail.com>
2016-02-09 19:09:57 +0530

    update

--- Si2chipCloud/admin.py
+++ Si2chipCloud/admin.py
@@ -7,6 +7,21 @@ from .forms import TempMeasureForm
 from .models import MotionMeasure
 from .forms import MotionMeasureForm
 
+from .models import HumidityMeasure
+
+from .models import LigthIntesityMeasure
+
+from .models import CameraState
+
+from .models import MainGateState
+
+from .models import AcSatte
+
+from .models import TvState
+
+from .models import WashingMachineState
+
+
 class TempMeasureAdmin(admin.ModelAdmin):
     list_display = ["temperature","timestamp"]
     forms = TempMeasure
@@ -22,7 +37,12 @@ class MotionMeasureAdmin(admin.ModelAdmin):
         #code
     
 
-
-
 admin.site.register(TempMeasure, TempMeasureAdmin)
 admin.site.register(MotionMeasure, MotionMeasureAdmin)
+admin.site.register(HumidityMeasure)
+admin.site.register(LigthIntesityMeasure)
+admin.site.register(CameraState)
+admin.site.register(MainGateState)
+admin.site.register(AcSatte)
+admin.site.register(TvState)
+admin.site.register(WashingMachineState)
--- Si2chipCloud/models.py
+++ Si2chipCloud/models.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 
 # Create your models here.
+
+#Temperature measurement class
 class TempMeasure(models.Model):
     #temperature = models.CharField(max_length=250, blank=False)
     temperature = models.FloatField(null=True, blank=True)
@@ -13,12 +15,72 @@ class TempMeasure(models.Model):
     def __unicode__(self):
         return str(self.temperature)
     #code
-
+#Motion measurement class
 class MotionMeasure(models.Model):
     motion = models.FloatField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add = True)
     
-    
     def __unicode__(self):
         return str(self.motion)
-    #code
+    
+    
+#humidity measurement class
+class HumidityMeasure(models.Model):
+    humidity = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.humidity)
+    
+
+    
+#light intensity measurement class
+class LigthIntesityMeasure(models.Model):
+    light_intensity = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.light_intensity)
+
+#Camera state acknowledgement
+class CameraState(models.Model):
+    camera = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.camera)
+      
+    
+#Main gate  state acknowledment class
+class MainGateState(models.Model):
+    main_gate = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.main_gate)
+
+
+#AC state acknowledment class
+class AcSatte(models.Model):
+    air_conditioner = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.air_conditioner)
+
+#TV state acknowlegdement class  
+class TvState(models.Model):
+    tv_state = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.tv_state)
+
+
+#Washing Machine ackknowledgement class
+class WashingMachineState(models.Model):
+    washing_machine = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return str(self.washing_machine)
